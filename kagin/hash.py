9e941fe370ff993b2368b31ff3c078db8da8df0f
Victor Lin <bornstub@gmail.com>
2011-08-30 20:57:41 +0800

    Initialize the project

---
+++ kagin/hash.py
@@ -0,0 +1,68 @@
+import os
+import logging
+import hashlib
+import shutil
+
+class HashFile(object):
+    """This object hashes content files and output files with hash file name,
+    and generate a file map
+    
+    """
+    
+    def __init__(
+        self, 
+        input_dir, 
+        output_dir, 
+        hash_type=hashlib.md5, 
+        exclude_files=None, 
+        logger=None
+    ):
+        self.logger = logger
+        if self.logger is not None:
+            self.logger = logging.getLogger(__name__)
+        #: path to input directory
+        self.input_dir = input_dir
+        #: path to output directory
+        self.output_dir = output_dir
+        #: type of hash function to run, MD5 as default value
+        self.hash_type = hash_type
+        #: list of filename patterns to ignore
+        self.exclude_files = exclude_files
+        
+    def compute_hash(self, filename):
+        """Compute hash value of a file
+        
+        """
+        import mmap
+        hash = self.hash_type()
+        if not os.path.getsize(filename):
+            return hash.hexdigest()
+        chunk_size = 4096
+        with open(filename, 'rb') as file:
+            map = mmap.mmap(file.fileno(), 0, 
+                            access=mmap.ACCESS_READ)
+            while True:
+                chunk = map.read(chunk_size)
+                if not chunk:
+                    break
+                hash.update(chunk)
+        return hash.hexdigest()
+            
+    def run(self):
+        """Run file hashing, generate filename map and return
+        
+        """
+        file_map = {}
+        for _, _, filenames in os.walk(self.input_dir):
+            for filename in filenames:
+                # TODO: exclude files here
+                hash = self.compute_hash(filename)
+                _, ext = os.path.splitext(filename)
+                output_filename = os.path.join(self.output_dir, hash + ext)
+                shutil.copy(filename, output_filename)
+                file_map[filename] = output_filename
+        return file_map
+                
+if __name__ == '__main__':
+    h = HashFile('.', '.')
+    print h.run()
---
+++ kagin/minify.py
@@ -0,0 +1,81 @@
+import os
+import logging
+import subprocess
+
+class FileConfig(object):
+    """Group of files to be minified and compacted together
+    
+    """
+    def __init__(self, logger=None):
+        self.logger = logger
+        if self.logger is None:
+            self.logger = logging.getLogger(__name__)
+            
+        #: group of files
+        self.groups = {}
+        #: map from filename to group name
+        self.filename_map = {}
+        
+    def add_group(self, name, filenames):
+        """Add file group
+        
+        """
+        assert name not in self.groups 
+        self.groups[name] = filenames
+        for filename in filenames:
+            self.filename_map[filename] = name
+        
+    def include_files(self, filenames):
+        """Return name of groups which include files in filenames
+        
+        """
+        groups = set()
+        for filename in filenames:
+            group_name = self.filename_map[filename]
+            groups.add(group_name)
+        return groups
+    
+class Compressor(object):
+    
+    def __init__(self, logger=None):
+        self.logger = logger
+        if self.logger is None:
+            self.logger = logging.getLogger(__name__)
+            
+        # make sure java is installed
+        subprocess.check_output(['java', '-version'])
+    
+    def _get_yui_compressor(self):
+        """Get path of yui compressor
+        
+        """
+        import kagin
+        pkg_dir = os.path.dirname(kagin.__file__)
+        path = os.path.join(pkg_dir, 'yuicompressor-2.4.2.jar')
+        return path
+        
+    def minify(self, input_files, output_path):
+        minified = []
+        yui_path = self._get_yui_compressor()
+        for filename in input_files:
+            self.logger.info('Minifying %s ...', filename)
+            output = subprocess.check_output(['java', '-jar', 
+                                              yui_path, filename])
+            minified.append(output)
+        minified = '\n'.join(minified)
+        with open(output_path, 'wt') as file:
+            file.write(minified)
+    
+class Builder(object):
+    """Builder builds CSS or JS file groups into minified files
+    
+    """
+    def __init__(self, output_dir, ext):
+        self.output_dir = output_dir
+        self.ext = ext
+        
+    def build(self, file_config):
+        compressor = Compressor()
+        for name, filenames in file_config.groups.iteritems():
+            output_filename = os.path.join(self.output_dir, name + self.ext)
+            compressor.minify(filenames, output_filename)
