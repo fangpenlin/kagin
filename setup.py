7341cd485992eca370db9ea2dd5e4de8b0e01720
Victor Lin <bornstub@gmail.com>
2013-04-19 09:05:06 +0800

    Fix compressor is not included issue

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from setuptools import setup, find_packages
 
 extra = {}
 
-version = '0.3'
+version = '0.4'
 
 setup(
     name='kagin',
     version=version,
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
     ], **extra
 )
